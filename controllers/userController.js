9f0752dc369172c4a470bd5f3b88b70cd92aec9a
Joon Park <jsp220@gmail.com>
2022-10-25 12:58:01 -0700

    Minor edits to controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,7 +56,7 @@ const deleteUser = async (req, res) => {
     try {
         const response = await User.findOneAndDelete({ _id: req.params.userId });
         if (response) {
-            Thought.deleteMany({ _id: { $in: user.thoughts } });
+            // Thought.deleteMany({ _id: { $in: response.thoughts } });
             res.status(200).json({ message: 'User successfully deleted'});
         } else res.status(404).json({ message: 'User not found' });
     } catch(err) { res.status(500).json(err) };
